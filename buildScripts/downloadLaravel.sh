55327886bf22ed55f5502ddcbe83ec8795cdc5b8
jetilling <jetilling@hotmail.com>
2019-03-26 11:00:28 -0400

    Updated Laravel download to 5.8.5 due to security patch

--- buildScripts/downloadLaravel.sh
+++ buildScripts/downloadLaravel.sh
@@ -6,4 +6,4 @@ echo "---------------------------------------------------"
 echo "| Downloading Laravel in $1"
 echo "---------------------------------------------------"
 
-curl -L https://github.com/laravel/laravel/archive/v5.8.3.tar.gz | tar xz
+curl -L https://github.com/laravel/laravel/archive/v5.8.5.tar.gz | tar xz
