929fe790a849e0d3612026fc0a08b91f7fa55dae
jetilling <jetilling@hotmail.com>
2019-03-25 21:42:22 -0400

    added laravel opcache.ini file and fixed incorrect file name in postgres.dockerfile

--- buildScripts/copyDockerFiles.sh
+++ buildScripts/copyDockerFiles.sh
@@ -11,6 +11,7 @@ cp ~/go/src/github.com/jetilling/projectBuilder/laravel_files/web.dockerfile web
 cp ~/go/src/github.com/jetilling/projectBuilder/laravel_files/postgres.dockerfile postgres.dockerfile
 
 cp ~/go/src/github.com/jetilling/projectBuilder/laravel_files/vhost.conf vhost.conf
+cp ~/go/src/github.com/jetilling/projectBuilder/laravel_files/opcache.ini opcache.ini
 
 cp ~/go/src/github.com/jetilling/projectBuilder/laravel_files/create-testing-db_template.sql create-testing-db_template.sql
 cp ~/go/src/github.com/jetilling/projectBuilder/laravel_files/docker-compose_template.yml docker-compose_template.yml
--- laravel_files/postgres.dockerfile
+++ laravel_files/postgres.dockerfile
@@ -1,2 +1,2 @@
 FROM postgres:11
-COPY create-dragonfly-testing-db.sql /docker-entrypoint-initdb.d/
+COPY create-testing-db.sql /docker-entrypoint-initdb.d/
